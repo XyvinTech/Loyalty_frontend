4be8f519ae2f8f04e5cd395e17e1a0feb2844680
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-09-01 18:33:32 +0530

    arabic

--- src/components/User-Facing/ArabicCard.jsx
+++ src/components/User-Facing/ArabicCard.jsx
@@ -350,7 +350,7 @@ const ArabicCard = ({ streak, show }) => {
             ) : (
               <div className="mt-6 flex justify-center items-center pb-0">
                 <AppButton
-                  name={<>Yeh!! Enjoy the {user.membership} tier Benefits</>}
+                  name={<>نعم!! استمتع بمزايا الفئة الذهبية!</>}
                   onClick={() => navigateWithParams("/user/how-to/ar")}
                   variant={theme.variant}
                 />

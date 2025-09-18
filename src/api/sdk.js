0a675f8f1148ffc3c9fd582693f6ad5095a69271
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-09-18 18:06:33 +0530

    feat(arabic card)

--- src/api/sdk.js
+++ src/api/sdk.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const sdkUrl = "http://141.105.172.45:7733/api/api/v1";
+const sdkUrl = "http://141.105.172.45:3737/api/v1";
 // const sdkUrl = "http://localhost:3000/api/v1";
 
 // Create SDK API client

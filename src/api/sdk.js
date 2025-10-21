c346b86c8a3d28374bc76aa5460751d6c80acf7f
xyvintech <admin@xyvin.com>
2025-10-21 11:23:00 +0530

    Fix SDK URL by removing redundant 'api' segment in sdk.js.

--- src/api/sdk.js
+++ src/api/sdk.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const sdkUrl = "https://khedmahloyalty.oifcoman.com:3737/api/api/v1";
+const sdkUrl = "https://khedmahloyalty.oifcoman.com:3737/api/v1";
 // const sdkUrl = "http://localhost:3000/api/v1";
 
 // Create SDK API client
